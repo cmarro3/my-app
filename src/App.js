c867786650e29d0093fc11e649d43f2252453eb6
cmarro3 <christophermarroquin352@gmail.com>
2021-07-08 22:32:01 -0500

    Update two

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import { Component } from 'react';
 import './App.css';
 import Intro from './componets/intro/index'
+import 'whatwg-fetch';
+import { fetch } from 'whatwg-fetch';
 
 class App extends Component {
   state = {
     series:[]
   }
+
+  componentDidMount(){
+    fetch('https://catfact.ninja/fact')
+    .then((response) => response.json())
+    .then(json => this.setState({series:json}))
+  }
+  
   render(){
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title"> Tv Series List</h1>
+        <h1 className="App-title"> Random Cat Facts</h1>
       </header>
-     <Intro message = 'Here you can find all of the most loved series'/>
-     the length of the series is - {this.state.series.length}
+     <Intro message = 'Here you can find facts about cats'/>
+     the length of the series of cat facts is - {this.state.series.length}
     </div>
   );
  }
--- src/componets/intro/index.js
+++ src/componets/intro/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 const Intro = props => (
     <p>
      {props.message}
